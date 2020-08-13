d126f39cc319e4f900cc5c8d7970b4ee3eae4db8
Ravana <47528708+ravana69@users.noreply.github.com>
2020-08-13 22:39:36 +0530

    Create creation.py

---
+++ plugins/creation.py
@@ -0,0 +1,50 @@
+from pyrogram.errors.exceptions.bad_request_400 import YouBlockedUser
+
+from userge import userge, Message
+from userge.utils.exceptions import StopConversation
+
+
+@userge.on_cmd("cr", about={
+    'header': "Creation gives you user's last updated names and usernames.",
+    'flags': {
+        '-u': "To get Username history of a User"},
+    'usage': "{tr}cr [Reply to user]\n"
+             "{tr}cr -u [Reply to user]"})
+async def creation_(message: Message):
+    """ Get User's Created Date """
+    replied = message.reply_to_message
+    if not replied:
+        await message.err("```Reply to get creation History...```", del_in=5)
+        return
+    user = replied.from_user.id
+    chat = "@creationdatebot"
+    await message.edit("```Getting info, Wait plox ...```")
+    msgs = []
+    ERROR_MSG = "For your kind information, you blocked @creationdatebot, Unblock it"
+    try:
+        async with userge.conversation(chat) as conv:
+            try:
+                await conv.send_message("/id {}".format(user))
+            except YouBlockedUser:
+                await message.err(f"**{ERROR_MSG}**", del_in=5)
+                return
+            msgs.append(await conv.get_response(mark_read=True))
+            msgs.append(await conv.get_response(mark_read=True))
+            msgs.append(await conv.get_response(timeout=3, mark_read=True))
+    except StopConversation:
+        pass
+    message = "Message"
+    username = "Username History"
+    for msg in msgs:
+        if '-u' in message.flags:
+            if msg.text.startswith("No records found"):
+                await message.edit("```User is alien...```", del_in=5)
+                return
+            if msg.text.startswith(username):
+                await message.edit(f"`{msg.text}`")
+        else:
+            if msg.text.startswith("No records found"):
+                await message.edit("```User is alien...```", del_in=5)
+                return
+            if msg.text.startswith(message):
+                await message.edit(f"`{msg.text}`")
