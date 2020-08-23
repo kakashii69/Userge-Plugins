f860c096423d803f282d5cc0b9bedea976c84087
Ravana <47528708+ravana69@users.noreply.github.com>
2020-08-23 16:11:22 +0530

    Update autobio.py

--- plugins/autobio.py
+++ plugins/autobio.py
@@ -8,13 +8,21 @@ import asyncio
 from pyrogram.errors import FloodWait
 
 from resources.quotes import ENGLISH_QUOTES, HINDI_QUOTES
-from userge import userge, Message
+from userge import userge, Message, get_collection
 
 BIO_UPDATION = False
-AUTOBIO_TIMEOUT = 300
+AUTOBIO_TIMEOUT = 86400
+USER_DATA = get_collection("CONFIGS")
 
 CHANNEL = userge.getCLogger(__name__)
 LOG = userge.getLogger(__name__)
+
+
+async def _init() -> None:
+    global BIO_UPDATION  # pylint: disable=global-statement
+    data = await USER_DATA.find_one({'_id': 'BIO_UPDATION'})
+    if data:
+        BIO_UPDATION = data['on']
 
 
 @userge.on_cmd("autobio", about={
@@ -22,12 +30,14 @@ LOG = userge.getLogger(__name__)
     'usage': "{tr}autobio (for eng)\n{tr}autobio Hi (for hindi)"})
 async def auto_bio(msg: Message):
     """ Auto Update Your Bio """
-
-    global BIO_UPDATION
+    global BIO_UPDATION  # pylint: disable=global-statement
     if BIO_UPDATION:
         if isinstance(BIO_UPDATION, asyncio.Task):
             BIO_UPDATION.cancel()
         BIO_UPDATION = False
+        USER_DATA.update_one({'_id': 'BIO_UPDATION'},
+                             {"$set": {'on': False}}, upsert=True)
+        await asyncio.sleep(1)
 
         await msg.edit(
             "Auto Bio Updation is **Stopped** Successfully...", log=__name__, del_in=5)
@@ -41,16 +51,17 @@ async def auto_bio(msg: Message):
         await msg.err("Given Input is Invalid...")
         return
 
+    USER_DATA.update_one({'_id': 'BIO_UPDATION'},
+                         {"$set": {'on': True}}, upsert=True)
     await msg.edit(
         "Auto Bio Updation is **Started** Successfully...", log=__name__, del_in=3)
     BIO_UPDATION = asyncio.get_event_loop().create_task(autobio_worker(msg, bio_quotes))
 
 
 async def autobio_worker(msg: Message, bio_quotes: list):
-
     quotes = bio_quotes
     while BIO_UPDATION:
-        for k in range(29):
+        for k in range(30):
             if not BIO_UPDATION:
                 break
 
@@ -59,6 +70,7 @@ async def autobio_worker(msg: Message, bio_quotes: list):
 
             except FloodWait as s_c:
                 time.sleep(s_c.x)
+                CHANNEL.log(s_c)
             except Exception as e:
                 LOG.error(e)
 
